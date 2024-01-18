a9bf0f65b2a993588c33eb7535be14d0ba9fca45
osm <osm0822@naver.com>
2024-01-18 16:44:33 +0900

    스프링 프로젝트 초기화

---
+++ src/main/java/com/bagulbagul/bagulbagul/BagulbagulApplication.java
@@ -0,0 +1,13 @@
+package com.bagulbagul.bagulbagul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagulbagulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagulbagulApplication.class, args);
+	}
+
+}
