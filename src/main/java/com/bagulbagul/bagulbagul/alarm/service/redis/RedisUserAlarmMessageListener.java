5607eb5a8b1f41dcf0af9dc1df27ac465ee20ed2
osm <osm0822@naver.com>
2024-01-24 01:54:54 +0900

    feat: 실시간 유저 알람 서비스 Redis구현 추가

---
+++ src/main/java/com/bagulbagul/bagulbagul/alarm/service/redis/RedisUserAlarmMessageListener.java
@@ -0,0 +1,25 @@
+package com.bagulbagul.bagulbagul.alarm.service.redis;
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import reactor.core.publisher.Sinks.EmitResult;
+import reactor.core.publisher.Sinks.Many;
+
+public class RedisUserAlarmMessageListener implements MessageListener {
+    private Many sink;
+    private final int RETRY_CNT = 2;
+
+    public RedisUserAlarmMessageListener(Many sink) {
+        this.sink = sink;
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        int failCnt = 0;
+        EmitResult emitResult = sink.tryEmitNext(message.getBody());
+        while(emitResult.isFailure() && failCnt < RETRY_CNT) {
+            failCnt += 1;
+            sink.tryEmitNext(message.getBody());
+        }
+    }
+}
