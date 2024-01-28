11cdb1d05ca0e74c862f254cbcb4217487b57021
osm <osm0822@naver.com>
2024-01-28 16:38:03 +0900

    feat: OncePerRequestFilter를 WebFilter로 변경

--- src/main/java/com/bagulbagul/bagulbagul/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/bagulbagul/bagulbagul/security/filter/JwtAuthenticationFilter.java
@@ -4,50 +4,53 @@ import com.bagulbagul.bagulbagul.security.exception.AccessTokenException;
 import com.bagulbagul.bagulbagul.security.exception.RefreshTokenException;
 import com.bagulbagul.bagulbagul.security.jwt.JwtCookieService;
 import com.bagulbagul.bagulbagul.security.jwt.JwtProvider;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.web.server.context.ReactorContextWebFilter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter implements WebFilter {
 
     private final JwtProvider jwtProvider;
     private final JwtCookieService jwtCookieService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        authenticate(request, response);
-        filterChain.doFilter(request, response);
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return authenticate(exchange.getRequest(), exchange.getResponse())
+                .flatMap(userId -> {
+                    AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userId,
+                            null,
+                            AuthorityUtils.NO_AUTHORITIES
+                    );
+                    SecurityContext securityContext = new SecurityContextImpl(authenticationToken);
+                    return chain.filter(exchange).contextWrite(Context.of(SecurityContext.class, Mono.just(securityContext)));
+                });
     }
 
-    /*
-     * Cookie에 있는 Access Token을 검증 후 userId를 추출해서 인증 처리
-     * 만약 AccessToken이 만료되었다면 RefreshToken을 검증 후 토큰을 재발급
-     * 만약 RefreshToken도 만료되었다면 인증 실패
-     */
-    private void authenticate(HttpServletRequest request, HttpServletResponse response) {
+    private Mono<Long> authenticate(ServerHttpRequest request, ServerHttpResponse response) {
         //AccessToken 추출
         String accessToken = jwtCookieService.getAccessToken(request);
         log.debug("access token = {}", accessToken);
 
         //AccessToken 검증 후 userId 추출 시도
-        Long userId;
         try {
-            userId = jwtProvider.getUserIdFromAccessToken(accessToken);
+            return Mono.just(jwtProvider.getUserIdFromAccessToken(accessToken));
         }
         //실패 시 RefreshToken 검증 후 userId 추출 시도
         catch (AccessTokenException ae) {
@@ -55,23 +58,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             log.debug("refresh token = {}", refreshToken);
             //RefreshToken 검증 후 userId 추출 시도
             try {
-                userId = jwtProvider.getUserIdFromRefreshToken(refreshToken);
+                return Mono.just(jwtProvider.getUserIdFromRefreshToken(refreshToken));
             }
             //RefreshToken도 없다면 인증 실패
             catch (RefreshTokenException re) {
-                return;
+                return Mono.empty();
             }
         }
-
-        log.debug("인증 완료, userId = {}", userId);
-        //인증 처리를 위해 security context설정
-        AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                userId,
-                null,
-                AuthorityUtils.NO_AUTHORITIES
-        );
-        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-        securityContext.setAuthentication(authenticationToken);
-        SecurityContextHolder.setContext(securityContext);
     }
 }
